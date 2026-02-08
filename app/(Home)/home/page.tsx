66acb7273583a308993514c0c5beeafe67d0804f
Zeref101 <shreyas.mohanty03@gmail.com>
2026-02-08 21:41:15 +0530

    fix: Disable prerendering for /home

--- app/(Home)/home/page.tsx
+++ app/(Home)/home/page.tsx
@@ -2,6 +2,8 @@ import AboutMe from '@/components/about-me'
 import Intro from '@/components/Intro'
 import Projects from '@/components/projects'
 
+export const dynamic = "force-dynamic";
+
 const Page = () => {
     return (
         <div className='flex flex-col h-full items-center justify-center overflow-x-hidden'>
