193d0d7158b653ff32bdc11240a9a7384a078f7f
Zeref101 <shreyas.mohanty03@gmail.com>
2026-02-14 13:40:04 +0530

    fix:metadata fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-export default function Home() {
-  return (
-    <>
-      <div className="flex min-h-screen flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        Home
-      </div>
-    </>
-  );
+import { redirect } from "next/navigation";
+
+export default function Page() {
+  redirect("/home");
 }
--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   turbopack: {},
 
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/home",
-        permanent: false,
-      },
-    ];
-  },
-
   images: {
     remotePatterns: [
       {
